22e5dd5076ae19bb7ca740ec0ab3507d6427f9b6
Charu277 <73598687+Charu277@users.noreply.github.com>
2021-08-21 13:40:21 +0530

    Add files via upload

---
+++ CopyFile.py
@@ -0,0 +1,6 @@
+import shutil
+import os
+source="C:/Users/k/Downloads/python projects/functions pro-98/file1.txt"
+destination="C:/Users/k/Downloads/python projects/functions pro-98/file2.txt"
+dest=shutil.copy(source,destination)
+print("copied successfully")
